faef6ad07d32d290d68d03b9a69c8337cadc867a
Oswaldo Perozo <oswaldo.perozo@gmail.com>
2022-09-13 16:42:02 -0300

    Primer HW

--- 01-Node/homework/bash.js
+++ 01-Node/homework/bash.js
@@ -10,7 +10,6 @@ const done = function (output){
 // El evento stdin 'data' se dispara cuando el user escribe una línea
 process.stdin.on('data', function (data) {
   var args =data.toString().trim().split(' ');
-  //process.stdout.write(args);
   var cmd = args.shift();
   if (commands[cmd]){
     commands[cmd](args, done);
@@ -19,27 +18,3 @@ process.stdin.on('data', function (data) {
     process.stdout.write('\nOswal >');
   }
 });
-    //  var cmd = data.toString().trim(); // remueve la nueva línea
-//    if(cmd === 'date') {
-//         commands.date(data);
-
-//     //     process.stdout.write(Date());  
-//    }
-//    if(cmd === 'pwd') {
-//     commands.pwd(data);
-//     }
-//     if(cmd === 'ls') {
-//         commands.ls(data);
-//         }
-
-//   process.stdout.write('\nprompt > ');
-// });
-//commands[cmd];
-//})
-
-
-// const commands = require('./commands/index.js');
-
-// const cmd = 'date';
-
-// commands[cmd]()
